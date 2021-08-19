f4ca95dd47ffd9efc1471885fac35c92058381a0
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-08-19 09:50:51 +0300

    added new table delete

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -9,8 +9,13 @@ public class DatabaseRule extends ExternalResource{
     @Override
     protected void after() {
         try(Connection con = DB.sql2o.open()) {
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
             String deleteAnimalsQuery = "DELETE FROM animals *;";
-            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            String deleteRangersQuery = "DELETE FROM rangers *;";
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+            con.createQuery( deleteAnimalsQuery).executeUpdate();
+            con.createQuery( deleteAnimalsQuery).executeUpdate();
+
         }
     }
 }
